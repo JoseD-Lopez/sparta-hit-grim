3a8ef73d1e1c2e4a9ed4ef3e67e975743eb90b28
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-02-26 19:21:55 +0000

    added a new css and image files

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 
 
-
-
-  var word  = document.getElementById("uname");
-  var nameSubmited;
+	var myh3 = document.getElementById("results");
+  var myh2 = document.getElementById("results2");
+  var word  = document.getElementById("inputname");
   var div1 = document.getElementById('initpage'),
   div2 = document.getElementById('gamecontainer');
+  var start = document.getElementById('startGame');
+  start.addEventListener('click', startGame);
+  var stop = document.getElementById('stopGame');
+  stop.addEventListener('click', stopGame);
+
+
+  var nameDisplay = "";
+
 
 function switchVisible() {
   if(!initpage) return;
@@ -23,13 +30,22 @@ document.getElementById('Button1').addEventListener('click', switchVisible);
 
 document.querySelector('form.wordform').addEventListener("submit", function(event){
    event.preventDefault();
-   var value = word.value;
-   sendname(value);
+   nameDisplay = word.value;
+   sendname(nameDisplay);
+   sendname2(nameDisplay);
    word.value="";
 		 });
 
+
 function sendname(message){
-	nameSubmited = message;
-	console.log(nameSubmited);
+
+	myh3.innerHTML='Spartan! '+ message +' Get ready';
 }
+
+function sendname2(message){
+
+	myh2.innerHTML='Attack ' + message + '!';
+  
+}
+
 
