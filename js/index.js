1921d0856547f76fe75387c9d58458fed74eca71
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-02-26 14:30:38 +0000

    created a new css file

--- js/index.js
+++ js/index.js
@@ -3,9 +3,23 @@
 
 
   var word  = document.getElementById("uname");
-  var div1 = document.getElementById('initpage'),
-  var div2 = document.getElementById('gamecontainer');
   var nameSubmited;
+  var div1 = document.getElementById('initpage'),
+  div2 = document.getElementById('gamecontainer');
+
+function switchVisible() {
+  if(!initpage) return;
+  if (getComputedStyle(div1).display == 'block') {
+    initpage.style.display = 'none';
+    gamecontainer.style.display = 'block';
+  } else {
+    initpage.style.display = 'block';
+    gamecontainer.style.display = 'none';
+  }
+}
+document.getElementById('Button1').addEventListener('click', switchVisible);
+
+
 
 document.querySelector('form.wordform').addEventListener("submit", function(event){
    event.preventDefault();
@@ -19,15 +33,3 @@ function sendname(message){
 	console.log(nameSubmited);
 }
 
-
-function switchVisible() {
-  if(!initpage) return;
-  if (getComputedStyle(div1).display == 'block') {
-    initpage.style.display = 'none';
-    gamecontainer.style.display = 'block';
-  } else {
-    initpage.style.display = 'block';
-    gamecontainer.style.display = 'none';
-  }
-}
-document.getElementById('Button1').addEventListener('click', switchVisible);
