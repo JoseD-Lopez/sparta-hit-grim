1f2e2a8ff103ca8031cfc2c483646fd17f2f5013
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-02-26 20:13:04 +0000

    made some functionality in the js file

--- js/index.js
+++ js/index.js
@@ -1,17 +1,34 @@
 
-
-	var myh3 = document.getElementById("results");
+  var myh3 = document.getElementById("results");
   var myh2 = document.getElementById("results2");
   var word  = document.getElementById("inputname");
   var div1 = document.getElementById('initpage'),
   div2 = document.getElementById('gamecontainer');
+
+  const bushes = document.querySelectorAll('.bush');
+  var scoreBoard = document.querySelector('.score');
+  const finished = document.querySelector('.final');
+  const grims = document.querySelectorAll('.grim');
+
   var start = document.getElementById('startGame');
   start.addEventListener('click', startGame);
   var stop = document.getElementById('stopGame');
   stop.addEventListener('click', stopGame);
+  var 
 
 
   var nameDisplay = "";
+
+
+		function randomTime(min, max) {
+			return Math.round(Math.random() * (max - min) + min);
+		} 
+
+		function randomBush(bushes){
+			const idx = Math.floor(Math.random() * bushes.length);
+
+			const bush = bushes[idx];
+		}
 
 
 function switchVisible() {
@@ -39,7 +56,7 @@ document.querySelector('form.wordform').addEventListener("submit", function(even
 
 function sendname(message){
 
-	myh3.innerHTML='Spartan! '+ message +' Get ready';
+	myh3.innerHTML='Spartan! '+ message +' Get ready to meet the Grim!';
 }
 
 function sendname2(message){
