ada73cf691360832998823c6a3389939890f1fb2
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-02-26 14:24:30 +0000

    added the markup of the games architecture

--- js/index.js
+++ js/index.js
@@ -3,10 +3,31 @@
 
 
   var word  = document.getElementById("uname");
+  var div1 = document.getElementById('initpage'),
+  var div2 = document.getElementById('gamecontainer');
+  var nameSubmited;
 
 document.querySelector('form.wordform').addEventListener("submit", function(event){
    event.preventDefault();
    var value = word.value;
-   console.log(value);
+   sendname(value);
    word.value="";
 		 });
+
+function sendname(message){
+	nameSubmited = message;
+	console.log(nameSubmited);
+}
+
+
+function switchVisible() {
+  if(!initpage) return;
+  if (getComputedStyle(div1).display == 'block') {
+    initpage.style.display = 'none';
+    gamecontainer.style.display = 'block';
+  } else {
+    initpage.style.display = 'block';
+    gamecontainer.style.display = 'none';
+  }
+}
+document.getElementById('Button1').addEventListener('click', switchVisible);
